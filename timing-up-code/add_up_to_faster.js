9f699e9cda58edd9f12ea5c0b3a3b55fb2763eea
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-03-20 18:10:42 -0400

    added code learning about Timing different code

---
+++ timing-up-code/add_up_to_faster.js
@@ -0,0 +1,11 @@
+// Suppose we want to write a function that calculates the sum of all numbers from 1 up to (and including ) some number n //
+
+
+function addUpTo(n) {
+  return n * (n + 1) / 2;
+}
+
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
---
+++ timing-up-code/add_up_to_slower.js
@@ -0,0 +1,15 @@
+// Suppose we want to write a function that calculates the sum of all numbers from 1 up to (and including ) some number n //
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+console.log(addUpTo(6));
+
+
+// var t1 = performance.now();
+// addUpTo(1000000000);
+// var t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
